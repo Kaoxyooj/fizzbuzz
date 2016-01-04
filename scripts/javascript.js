7c24ca1970b33c586b6e9c9a78ad44009323cd9a
Kaoxyooj <locksage@hotmail.com>
2016-01-04 15:40:01 -0600

    1stcommit

---
+++ scripts/javascript.js
@@ -0,0 +1,14 @@
+"use strict";
+var num;
+
+for (num = 0; num <101; num++){
+	if (num % 3 === 0 && num % 5 === 0) {
+		console.log("fizzbuzz");
+	}else if (num % 5 === 0) {
+		console.log("buzz");
+	}else if (num % 3 === 0) {
+		console.log("fizz");
+	}else{
+		console.log(num);
+	}
+}
